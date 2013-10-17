cc414992cff760ac7aca75cd5746b4c37dde8829
fwelland <fwelland@localhost>
2013-10-17 09:27:32 -0400

    Initial Add of this project.

---
+++ Shared/src/fhw/qaulifiers/UserDatabase.java
@@ -0,0 +1,15 @@
+package fhw.qaulifiers;
+import java.lang.annotation.Target;
+import javax.inject.Qualifier;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+@Qualifier
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, PARAMETER, TYPE})
+public @interface UserDatabase {
+    
+}
